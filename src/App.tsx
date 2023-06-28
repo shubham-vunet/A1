72a0376ec89c6c349baabe8d762052dde90f1872
Shubham Sharma <10323297+shubham-vunet@users.noreply.github.com>
2023-06-28 13:59:07 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  // show a Grid which contains 10elements in each row,
+  // and show numbers from 0 to 9, 10 to 11, 20 to 29 in each row
+  // Bonus If they are square
+
+  return (
+    <div className="App">
+      <div style={{ background: "green" }}>0</div>
+      <div style={{ background: "green" }}>1</div>
+      <div style={{ background: "green" }}>2</div>
+    </div>
+  );
+}
