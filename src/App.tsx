417b28340844fb4f03ddc7680a74179db4070a68
Shubham Sharma <10323297+shubham-vunet@users.noreply.github.com>
2023-07-14 15:15:45 +0530

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,27 @@ export default function App() {
   // show a Grid which contains 10elements in each row,
   // and show numbers from 0 to 9, 10 to 11, 20 to 29 in each row
   // Bonus If they are square
-
+  const count = 10;
   return (
     <div className="App">
-      <div style={{ background: "green" }}>0</div>
-      <div style={{ background: "green" }}>1</div>
-      <div style={{ background: "green" }}>2</div>
-      <div style={{ background: "green" }}>3</div>
-      <div style={{ background: "green" }}>4</div>
-      <div style={{ background: "green" }}>5</div>
-      <div style={{ background: "green" }}>6</div>
-      <div style={{ background: "green" }}>7</div>
-      <div style={{ background: "green" }}>8</div>
-      <div style={{ background: "green" }}>9</div>
+      <div>
+        <div>
+          <h1>Numbers</h1>
+          <input value={count} />
+        </div>
+        <div className="numbers">
+          <div>0</div>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+          <div>8</div>
+          <div>9</div>
+        </div>
+      </div>
     </div>
   );
 }
