fc899f8db283e78e5bfbeb4644741aa36c6c9e46
Shubham Sharma <10323297+shubham-vunet@users.noreply.github.com>
2023-09-19 14:52:00 +0530

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Clock } from './Clock';
 
 // Add routing such that `/grid` shows grid component and `/clock` shows clock
 export function App() {
-  const count = 10;
   return (
     <div className="App">
       <Grid />
